acb6882b75d4a55237cb859270ddb4de63216e2d
Camilla Rodrigues <camilla.rodrigues.de.almeida@gmail.com>
2024-10-08 11:03:15 -0300

    feat backend filtrar produto

--- backend/src/main/java/com/github/camillara/confeitisys/model/repositories/ProdutoRepository.java
+++ backend/src/main/java/com/github/camillara/confeitisys/model/repositories/ProdutoRepository.java
@@ -1,6 +1,8 @@
 package com.github.camillara.confeitisys.model.repositories;
 
 import com.github.camillara.confeitisys.model.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +13,8 @@ import java.util.List;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    boolean existsByItensProdutoItemProdutoId(Long id);
+    @Query("SELECT p FROM Produto p WHERE upper(p.nome) like upper(:nome)")
+    Page<Produto> buscarPorNome(@Param("nome") String nome, Pageable pageable);
 
     @Query("SELECT COUNT(ip) > 0 FROM ItemProduto ip WHERE ip.itemProduto.id = :produtoId")
     boolean isProdutoUsadoComoItem(@Param("produtoId") Long produtoId);
--- backend/src/main/java/com/github/camillara/confeitisys/rest/produtos/ProdutoController.java
+++ backend/src/main/java/com/github/camillara/confeitisys/rest/produtos/ProdutoController.java
@@ -11,18 +11,13 @@ import com.github.camillara.confeitisys.model.enums.Categoria;
 import com.github.camillara.confeitisys.rest.produtos.dto.ProdutoFormRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 import com.github.camillara.confeitisys.model.repositories.ProdutoRepository;
 
@@ -156,6 +151,14 @@ public class ProdutoController {
 				.collect(Collectors.toList());
 
 		return ResponseEntity.ok(produtoDTOs);
+	}
+
+	@GetMapping("/filtrar")
+	public Page<ProdutoFormRequestDTO> getLista(
+			@RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+			Pageable pageable) {
+		return repository.buscarPorNome("%" + nome + "%", pageable)
+				.map(ProdutoFormRequestDTO::fromModel);
 	}
 
 
