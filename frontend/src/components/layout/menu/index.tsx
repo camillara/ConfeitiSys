42320a23baa6638d599c6e463e71fa1301979696
Camilla Rodrigues <camilla.rodrigues.de.almeida@gmail.com>
2024-10-11 16:31:02 -0300

    Temporário.

--- frontend/src/components/layout/menu/index.tsx
+++ frontend/src/components/layout/menu/index.tsx
@@ -1,34 +1,70 @@
+import { useState } from "react";
 import Link from "next/link";
-
-export const Menu: React.FC = () => {
-  return (
-    <aside className="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
-      <p className="menu-label is-hidden-touch">Menu ConfeitiSys</p>
-      <ul className="menu-list">
-        <MenuItem href="/" label="Home" />
-        <MenuItem href="/consultas/produtos" label="Produtos" />
-        <MenuItem href="/consultas/clientes" label="Clientes" />
-        <MenuItem href="/vendas/nova-venda" label="Venda" />
-        <MenuItem href="/" label="Sair" />
-      </ul>
-    </aside>
-  );
-};
+import styled from "styled-components";
 
 interface MenuItemProps {
   href: string;
   label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
+const MenuItem: React.FC<MenuItemProps> = ({ href, label }) => {
   return (
     <li>
-      <Link href={props.href}>
-        <a>
-          <span className="icon"></span>
-          {props.label}
-        </a>
+      <Link href={href}>
+        <a>{label}</a>
       </Link>
     </li>
+  );
+};
+
+const MenuContainer = styled.aside<{ isMenuOpen: boolean }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: ${(props) => (props.isMenuOpen ? "250px" : "0")};
+  transition: width 0.3s ease;
+`;
+
+const MenuHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MenuList = styled.ul<{ isMenuOpen: boolean }>`
+  display: ${(props) => (props.isMenuOpen ? "block" : "none")};
+`;
+
+const ToggleButton = styled.button`
+  border: none;
+  background: none;
+  font-size: 24px;
+`;
+
+export const Menu: React.FC = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <MenuContainer isMenuOpen={isMenuOpen}>
+      <MenuHeader>
+        <p>Menu ConfeitiSys</p>
+        <ToggleButton onClick={toggleMenu}>
+          <span className="icon">
+            <i className={`fas fa-${isMenuOpen ? "times" : "bars"}`}></i>
+          </span>
+        </ToggleButton>
+      </MenuHeader>
+      <MenuList isMenuOpen={isMenuOpen}>
+        <MenuItem href="/" label="Home" />
+        <MenuItem href="/consultas/produtos" label="Produtos" />
+        <MenuItem href="/consultas/clientes" label="Clientes" />
+        <MenuItem href="/vendas/nova-venda" label="Venda" />
+        <MenuItem href="/" label="Sair" />
+      </MenuList>
+    </MenuContainer>
   );
 };
